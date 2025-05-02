f74543afb33f508d761d243036e987daf961851a
unknown <saidberchih@gmail.com>
2025-05-02 11:10:53 +0000

    get return Notes and invoices and save them in return_notes.db and invoices.db

--- puppeteer-script.js
+++ puppeteer-script.js
@@ -143,25 +143,6 @@ async function getReturnNotes(page) {
     });
     await page.select(CONFIG.RETURN_NOTES.SELECTORS.DROPDOWN, "50");
 
-    // const noteIds = await page.$$eval(
-    //   CONFIG.RETURN_NOTES.SELECTORS.ROWS,
-    //   (rows) => {
-    //     return rows
-    //       .filter((row) => {
-    //         // Get the 5th td element (index 4) which contains the parcel count
-    //         const parcelCountCell = row.cells[4];
-    //         const parcelCount = parseInt(parcelCountCell.textContent.trim());
-    //         return parcelCount > 0;
-    //       })
-    //       .map((row) => row.id)
-    //       .filter(Boolean);
-    //   }
-    // );
-
-    // console.log("notes");
-    // console.log(noteIds);
-    // console.log(noteIds.length);
-
     async function getUnsyncedReturnNotes(page) {
       try {
         const syncedReturnNoteIds = await getSyncedReturnNoteIds();
@@ -202,15 +183,15 @@ async function getReturnNotes(page) {
     const unsyncedReturnIds = await getUnsyncedReturnNotes(page);
     console.log(`unsyncedReturnIds : ${unsyncedReturnIds}`);
 
-    // for (const noteId of unsyncedReturnIds) {
-    //   try {
-    //     const noteDetails = await processReturnNote(page, noteId);
-    //     results.push(noteDetails);
-    //   } catch (error) {
-    //     console.error(`Error processing return note ${noteId}:`, error);
-    //     continue;
-    //   }
-    // }
+    for (const noteId of unsyncedReturnIds) {
+      try {
+        const noteDetails = await processReturnNote(page, noteId);
+        results.push(noteDetails);
+      } catch (error) {
+        console.error(`Error processing return note ${noteId}:`, error);
+        continue;
+      }
+    }
 
     return results;
   } catch (error) {
@@ -340,8 +321,8 @@ async function getInvoices(page) {
     await login(page);
 
     const returnNotes = await getReturnNotes(page);
-    // await saveReturnNotesToFirestore(returnNotes);
-    // await saveReturnNotesToSQLite(returnNotes);
+    await saveReturnNotesToFirestore(returnNotes);
+    await saveReturnNotesToSQLite(returnNotes);
 
     const invoices = await getInvoices(page);
     await saveInvoicesToSQLite(invoices);
